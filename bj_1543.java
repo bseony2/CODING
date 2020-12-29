7b0473fc8f4eb3aeed15a37c191167b256c7ef13
bseony2 <bseony2@gmail.com>
2020-12-30 00:29:17 +0900

    1543_문서 검색

---
+++ bj_1543.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class bj_1543 {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String dic = scan.nextLine();
+        String word = scan.nextLine();
+        int result = 0;
+
+        for(int i = 0; word.length() <= dic.length() - i; i++)
+        {
+            if(dic.substring(i, i + word.length()).equals(word))
+            {
+                result++;
+                i = i + word.length() - 1;
+            }
+            else continue;
+        }
+
+        System.out.println(result);
+    }
+}
