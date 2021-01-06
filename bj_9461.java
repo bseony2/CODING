14987a41bf3d2698e4980b08b12eb2e2826a73ac
bseony2 <bseony2@gmail.com>
2021-01-06 17:55:48 +0900

    파도반 수열_01/06

---
+++ bj_9461.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class bj_9461 {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        for(int i= 4; i<101;i++)
+            dp[i] = dp[i-2] + dp[i-3];
+        
+        int N = scan.nextInt();
+        for(int i=0; i<N;i++)
+        {
+            System.out.println(dp[scan.nextInt()]);
+        }
+    }
+}
