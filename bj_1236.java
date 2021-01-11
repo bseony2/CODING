d053ec91e05ec1567406241e697f6fa7084fee54
bseony2 <bseony2@gmail.com>
2021-01-11 18:45:50 +0900

    런타임 에러 해ㅐ결

--- bj_1236.java
+++ bj_1236.java
@@ -1,60 +1,50 @@
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException; 
-
 
 public class bj_1236 {
-    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();	// 세로
+		int M = scan.nextInt();	// 가로
+		int row = 0;	// 행
+		int col = 0;	// 열
+		char[][] map = new char[N][M];
+		for(int i=0; i<map.length; i++) {
+			String str = scan.next();
+			for(int j=0; j<map[i].length; j++) {
+				map[i][j] = str.charAt(j);
+			}
+		}
+		
+		// 행에 필요한 경비원 수
+		for(int i=0; i<N; i++) {
+			boolean flag = true;
+			for(int j=0; j<M; j++) {
+				if(map[i][j] == 'X') {
+					flag = false;
+					break;
+				}
+			}
+			if(flag)
+				row ++;
+		}
+		
+		// 열에 필요한 경비원 수
+		for(int i=0; i<M; i++) {
+			boolean flag = true;
+			for(int j=0; j<N; j++) {
+				  if(map[j][i] == 'X') {
+					  flag = false;
+					  break;
+				  }
+			}
+			if(flag)
+				col ++;
+		}
+		
+		// 행, 열 중 더 많은 경비원 추가
+		System.out.println(Math.max(row, col));
+		scan.close();
+	}
 
-        //입력 초기화 단계
-        String[] s = br.readLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int M = Integer.parseInt(s[1]);
-        char[][] castle = new char[N][M];
-        for(int i=0; i<N; i++)
-        {
-            castle[i] = br.readLine().toCharArray();
-        }
-        int row = N;
-        int column = M;
-
-        //행으로 계산 했을 시 필요한 경비원 row
-        for(int i=0; i<N; i++)
-        {
-            boolean guard = false;
-            for(int j=0; j<M; j++)
-            {
-                if(castle[i][j] == 'X')
-                {
-                    guard = true;
-                    break;
-                }
-            }
-            if(guard == true)
-                row--;
-        }
-
-        //열로 계산 했을 경우 필요한 경비원 column
-        for(int i=0; i<M; i++)
-        {
-            boolean guard = false;
-            for(int j=0; j<N; j++)
-            {
-                if(castle[j][i] == 'X')
-                {
-                    guard = true;
-                    break;
-                }
-            }
-            if(guard == true)
-                column--;
-        }
-        sc.close();
-        System.out.println(Math.max(row, column));
-    }
-    
 }
