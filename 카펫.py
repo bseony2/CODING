ffe372cd8d030bb80af6653cb72d7f4e81576d2f
bseony2 <bseony2@gmail.com>
2021-04-02 13:42:13 +0900

    brute force

--- 카펫.py
+++ 카펫.py
@@ -5,7 +5,7 @@ def solution(brown, yellow):
     while(True):
         y = h**2
         b = 4*h+4
-        w=1
+        w=h
 
         while(y <= yellow and b<=brown):
             if(b == brown and y == yellow):return [w+2, h+2]
