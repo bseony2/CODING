16c39d3d7718efc5c60cbeac9cd894639d002e87
bseony2 <bseony2@gmail.com>
2021-07-01 17:19:12 +0900

    방문길이_greedy

---
+++ 방문 길이.py
@@ -0,0 +1,13 @@
+def solution(dirs):
+    s = set()
+    d = {'U': (0,1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
+    x, y = 0, 0
+    for i in dirs:
+        nx, ny = x + d[i][0], y + d[i][1]
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            s.add((x,y,nx,ny))
+            s.add((nx,ny,x,y))
+            x, y = nx, ny
+    return len(s)//2
+
+print(solution("ULURRDLLU"))
