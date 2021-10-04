e102eb847373836ada73a21dcc69843505d46834
bseony2 <bseony2@gmail.com>
2021-10-04 19:53:14 +0900

    괄호변화_구현_파이써닉.... 조금 더 단순하고 효율적으로 재귀를 짜야....

---
+++ 괄호 변환.py
@@ -0,0 +1,46 @@
+def isCorrectString(string) : #균형잡힌 괄호 문자열
+    left = 0; right = 0;
+    if string[0] == ')' or string[-1] == '(': return False
+    for s in string : 
+        if s == '(' : left = left + 1
+        else : right = right + 1
+        if right > left : return False
+    if right == left : return True
+
+def isBalancedString(string) : 
+    if string.count('(') == string.count(')') : return True
+    else : return False
+
+def reversString(string) :
+    answer =''
+    for s in string :
+        if s == '(' : answer += (')')
+        else : answer += ('(')
+    return answer
+
+def construct(string) : 
+    if string == '' or None: return ''
+    answer = ''
+    for s in range(1, len(string) + 1) : 
+        if(isCorrectString(string[:s])) : #u가 올바른 문자열일 경우
+            answer += (string[:s])
+            constructResult = construct(string[s:])
+            answer += constructResult
+            return answer
+        elif(isBalancedString(string[:s])) : #u가 균형잡힌 문자열일 경우
+            #if string[:s] == ')(' : return '()'
+            blankString =''
+            blankString += ('(') 
+            blankString += construct(string[s:])
+            blankString += (')')
+            blankString += (reversString(string[1:s -1]))
+            return blankString
+    return answer
+
+def solution(string):
+    if(isCorrectString(string)) : return string
+    answer = construct(string)
+    return answer
+
+
+print(solution("()))((()"))
