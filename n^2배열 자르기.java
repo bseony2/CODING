39e67ba49e819d25656b5647c8ffee22f4928261
bseony2 <bseony2@gmail.com>
2021-10-18 11:03:30 +0900

    Revert "n^2 배열 자르기.java _ 그리디? 구현? _ 숨어있는 규칙 찾는게 약함;;"
    This reverts commit c78ea888fbfc8a5956971e027050125bf86d1f8f.

--- n^2배열 자르기.java
+++
@@ -1,9 +0,0 @@
-import java.util.LinkedList;
-
-class Solution {
-    public int[] solution(int n, long left, long right) {
-        LinkedList<Integer> answer = LinkedList<Integer>();
-        int[] answer = {};
-        return answer;
-    }
-}
