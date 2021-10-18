c78ea888fbfc8a5956971e027050125bf86d1f8f
bseony2 <bseony2@gmail.com>
2021-10-18 10:58:40 +0900

    n^2 배열 자르기.java _ 그리디? 구현? _ 숨어있는 규칙 찾는게 약함;;

---
+++ n^2배열 자르기.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        LinkedList<Integer> answer = LinkedList<Integer>();
+        int[] answer = {};
+        return answer;
+    }
+}
