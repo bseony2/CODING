778f58bdb70c5cf94a58a89dda027ba16f12f6c9
bseony2 <bseony2@gmail.com>
2022-01-26 21:00:30 +0900

    고유한 이메일 _ 문자열 처리

---
+++ 고유한이메일.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class 고유한이메일{
+	static StringBuilder sb;
+	static Set<String> set = new HashSet<String>();
+	
+	public static void main(String[] args) {
+		proceed("test.email+james@coding.com");
+		proceed("test.e.mail+toto.jane@coding.com");
+		proceed("testemail+tom@cod.ing.com");
+		
+		System.out.println(set.size());
+	}
+	
+	static public void proceed(String email) {
+		int splitPoint = email.indexOf('@');
+		sb = new StringBuilder(splitPoint);
+		String local = email.substring(0, splitPoint + 1);
+		for(int i=0; i<local.length(); i++)
+		{
+			if(local.charAt(i) == '.')
+				continue;
+			else if(local.charAt(i) == '+')
+				break;
+			else
+				sb.append(local.charAt(i));
+		}
+		sb.append(email.substring(splitPoint + 1, email.length()));
+		set.add(sb.toString());
+	}
+	
+}
