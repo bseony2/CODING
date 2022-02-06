36dd377a00c0c5091761363f02b11c706777d571
bseony2 <bseony2@gmail.com>
2022-02-06 19:47:03 +0900

    빛의경로사이클.java : 그래프 구현현

---
+++ 빛의경로사이클.java
@@ -0,0 +1,72 @@
+import java.util.LinkedList;
+import java.util.Collections;
+public class 빛의경로사이클 {
+
+  public static void main(String[] args) {
+    String[] grid = {"SL","LR"};
+    LinkedList answer = new 빛의경로사이클().solution(grid);
+    for(int i=0; i<answer.size(); i++)
+    {
+      System.out.println(answer.get(i));
+    }
+  }
+
+  static int height, width;
+  static boolean[][][] isVisited;
+  static String[] graph;
+  
+  static public LinkedList<Integer> solution(String[] grid) {
+      height = grid.length;
+      width = grid[0].length();
+      graph = grid;
+      isVisited = new boolean[height][width][4];
+      
+      LinkedList<Integer> answer = new LinkedList<Integer>();
+      
+      for(int i=0; i<height; i++)
+      {
+          for(int j=0; j<width; j++)
+          {
+              for(int k = 0; k<4; k++)
+              {
+                  if(!isVisited[i][j][k])
+                      answer.add(check(i, j, k));
+              }
+          }
+      }
+      Collections.sort(answer);
+      return answer;
+  }
+  
+  static int check(int y, int x, int k) //y좌표, x좌표, 방향
+  {
+      int[] dy = {-1, 0, 1, 0}; //위, 오른쪽, 아래, 왼쪽
+      int[] dx = {0, 1, 0, -1};
+      int result = 0;
+      while(!isVisited[y][x][k])
+      {
+          result++;
+          isVisited[y][x][k] = true;
+          
+          //다음 좌표 지정
+          y = (y + dy[k] + height) % height;
+          x = (x + dx[k] + width) % width;
+          
+          //다음 좌표의 방향 지정(k)
+          //가는 방향 지정 S이면 그냥 같은 방향
+          if(graph[y].charAt(x) == 'L')
+              k = (k + 3) % 4;
+              //k가 0이었으면 3
+              //k가 1이었으면 0
+              //k가 2었으면 1
+              //k가 3이었으면 2
+          else if(graph[y].charAt(x) == 'R')
+              //k가 0이었으면 1
+              //k가 1이었으면 2
+              //k가 2었으면 3
+              //k가 3이었으면 0
+              k = (k + 1) % 4;
+      }
+      return result;
+  }
+}
